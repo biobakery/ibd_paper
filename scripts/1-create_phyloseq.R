92587b344e8afc04085c7a64b0eeee244f85eb79
Siyuan Ma <sim653@mail.harvard.edu>
2018-10-16 12:23:58 -0400

    Check lib sizes

--- scripts/1-create_phyloseq.R
+++ scripts/1-create_phyloseq.R
@@ -1,8 +1,10 @@
 .rs.restartR()
-library(magrittr)
+dir.create("results/1-create_phyloseq/", recursive = TRUE, showWarnings = FALSE)
 source("scripts/misc/constants.R")
+source("scripts/misc/helpers.R")
 
-# load the biom files, some initial QC
+# Load the biom files, some initial QC ------------------------------------
+
 l_biom <- list()
 for(study in studies) {
   print(study)
@@ -11,15 +13,63 @@ for(study in studies) {
                  study, 
                  "/16s/all_samples_taxonomy_closed_reference.biom") %>% 
     phyloseq::import_biom()
-  
-  # all otu clusters should have at least some presence in the data,
-  # based on the pipeline
   if(any(phyloseq::taxa_sums(biom) == 0)) 
-    stop("There are zero sum taxa in the data!")
+    stop("There are zero sum taxa in the data!") # all otu clusters should have at least some presence 
+                                                 # in the data, based on the pipeline
   l_biom[[study]] <- biom
-  
-  # 
 }
+
+# Some lib size sanity check
+lib_size <- studies %>% 
+  purrr::map_dfr(~ tibble::tibble(
+    study = .x,
+    sample = l_biom %>% 
+      extract2(.x) %>% 
+      phyloseq::sample_names(),
+    lib.size = l_biom %>% 
+      extract2(.x) %>% 
+      phyloseq::sample_sums()
+  ))  
+read_counts <- studies %>% 
+  purrr::map_dfr(~ paste0(dir_processed, 
+                          "processed/", 
+                          .x, 
+                          "/16s/all_samples_read_counts.tsv") %>% 
+                   readr::read_tsv(col_types = "cddd") %>% 
+                   dplyr::mutate(study = .x)) 
+lib_size %>% 
+  dplyr::filter(sample %>% 
+                  is_in(read_counts$`# sample`) %>% 
+                  not)
+read_counts %>%
+  dplyr::filter(`# sample` %>% 
+                  is_in(lib_size$sample) %>% 
+                  not) # samples that aren't present in the OTU table 
+                       # are because of low read counts
+lib_size <- lib_size %>% 
+  dplyr::left_join(read_counts,
+                   c("study" = "study",
+                     "sample" = "# sample"))
+lib_size %>% 
+  ggplot(aes(x = lib.size, y = `reads mapping to OTU with taxonomy`)) +
+  geom_point() +
+  facet_wrap(~study) # the two are equal
+lib_size %>% 
+  tidyr::gather(key = "Type of read count",
+                value = "N",
+                "original read count",
+                "lib.size",
+                `reads mapping to OTU with taxonomy`,
+                `reads mapping to unclassifed OTU`,
+                factor_key = TRUE) %>% 
+  ggplot(aes(x = study, y = log10(N + 1), color = `Type of read count`)) +
+  geom_boxplot() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggsave(filename = "results/1-create_phyloseq/fig_libSize.pdf",
+       width = 10,
+       height = 8)
+
+# Create phyloseq objects -------------------------------------------------
 
 template <- paste0(dir_processed, "data/template.csv") %>% 
   readr::read_csv()
@@ -29,17 +79,17 @@ column_setup <- template$var.class %>%
                 "integer" = "i") %>% 
   paste(collapse = "")
 
-for(study in studies) {
-  print(study)
-  biom <- paste0(dir_processed, 
-                 "processed/", 
-                 study, 
-                 "/16s/all_samples_taxonomy_closed_reference.biom") %>% 
-    phyloseq::import_biom()
-  # some quality checks on the processed data
-  if(any(phyloseq::taxa_sums(biom) == 0)) 
-    stop("There are zero sum taxa in the data!")
-  # metadata <- paste0(dir_processed, 
+# for(study in studies) {
+#   print(study)
+#   biom <- paste0(dir_processed, 
+#                  "processed/", 
+#                  study, 
+#                  "/16s/all_samples_taxonomy_closed_reference.biom") %>% 
+#     phyloseq::import_biom()
+#   # some quality checks on the processed data
+#   if(any(phyloseq::taxa_sums(biom) == 0)) 
+#     stop("There are zero sum taxa in the data!")
+#   # metadata <- paste0(dir_processed, 
   #                    "processed/", 
   #                    study, 
   #                    "/metadata/metadata.txt") %>% 
---
+++ scripts/misc/helpers.R
@@ -0,0 +1,6 @@
+library(magrittr)
+
+# plotting
+library(ggplot2)
+library(cowplot)
+theme_set(theme_bw())
