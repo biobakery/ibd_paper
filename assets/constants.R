bced5bd557a350a4e6697586b2d8a8e04bff9e23
Siyuan Ma <sim653@mail.harvard.edu>
2018-11-01 07:33:13 -0400

    adjust for batch

--- assets/constants.R
+++ assets/constants.R
@@ -1,9 +1,13 @@
 lib.toLoad <- 
   c("magrittr", # for easy operators
-    "ggplot2", "cowplot", # for plotting
+    "ggplot2", "cowplot", "ggrepel", # for plotting
+    "DT", # for table formatting
     "phyloseq" # for phyloseq objects
   )
-lib.required <- c(lib.toLoad)
+lib.required <- c(lib.toLoad,
+                  "tidyverse",
+                  "hash", "optparse" # for Maaslin2
+                  )
 studies <- c("BIDMC-FMT",
              "CS-PRISM",
              "Herfarth_CCFA_Microbiome_3B_combined",
