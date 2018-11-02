e96fdb4931b9aaa01f4d5259e5dd9b4665e7aa03
Siyuan Ma <sim653@mail.harvard.edu>
2018-11-01 20:34:23 -0400

    Batch adjustment + distances

--- functions/helpers.R
+++ functions/helpers.R
@@ -1,6 +1,17 @@
 # utilities
-u.s <- function() {
-  R.utils::sourceDirectory("functions/")
+subset_distance <- function(dist, ind) {
+  if(class(dist) != "dist")
+    stop("dist must be a distance class!")
+  if(!(class(ind) %in% c("character", "logical")))
+    stop("ind must be a name/logical vector!")
+  
+  dist_matrix <- as.matrix(dist)
+  if(class(ind) == "logical" & nrow(dist_matrix) != length(ind))
+    stop("Dimensions of dist and ind must match!")
+  if(class(ind) == "character" & !all(ind %in% rownames(dist_matrix)))
+    stop("ind must be a subset of the row/col names of dist!")
+  
+  return(as.dist(dist_matrix[ind, ind]))
 }
 
 # plot
@@ -28,4 +39,28 @@ gg_color_hue <- function(values = NULL, n = NULL) {
   }
   hues <- seq(15, 375, length = n + 1)
   colors <- hcl(h = hues, l = 65, c = 100)[1:n]
+}
+gather_axes <- function(data, n_axis) {
+  if(n_axis %% 2 != 0)
+    stop("n_axis must be even number!")
+  tb_axes <- (1:(n_axis/2)) %>% 
+    purrr::map_dfr(function(i.axis) {
+      tibble::tibble(x.axis = data[, i.axis*2 - 1, drop = TRUE],
+                     y.axis = data[, i.axis*2, drop = TRUE],
+                     axes = paste0(i.axis*2 - 1, "vs.", i.axis*2))
+    })
+  if(!is.null(row.names(data)))
+    tb_axes$rowname <- rep(row.names(data), n_axis/2)
+  return(tb_axes)
+}
+extract_pcoa <- function(fit_pcoa, n_axis) {
+  varExplained <- fit_pcoa$values$Relative_eig[1:n_axis]
+  tb_axes <- gather_axes(fit_pcoa$vectors, n_axis)
+  tb_varExplained <- (1:(n_axis/2)) %>% 
+    purrr::map_dfr(function(i.axis) {
+      tibble::tibble(axes = paste0(i.axis*2 - 1, "vs.", i.axis*2),
+                     x.perc.var = varExplained[i.axis*2 - 1],
+                     y.perc.var = varExplained[i.axis*2])
+    })
+  tb_axes %>% dplyr::left_join(tb_varExplained, by = "axes") %>% return()
 }
