e00fa86298ac15a2aa07415a5c9e1e1b1661c2b2
Siyuan Ma <sim653@mail.harvard.edu>
2019-04-18 11:47:11 -0400

    bug in the permutation functions

--- functions/PERMANOVA_repeat_measures.R
+++ functions/PERMANOVA_repeat_measures.R
@@ -48,7 +48,7 @@ PERMANOVA_repeat_measures <- function(
     stop("subject number from subject and subject_data must match!")
   if(!setequal(subject, rownames(subject_data)))
     stop("subject identifiers from subject and subject_data must match!")
-
+  
   # create sample data if missing
   if(is.null(sample_data)) {
     sample_data <- data.frame(place_holder_sample = rep(1, nrow(as.matrix(D))),
@@ -72,11 +72,6 @@ PERMANOVA_repeat_measures <- function(
     stop("metadata_order contains metadata not in subject_data and sample_data!")
   }
   
-  library(vegan)
-  library(permute)
-  library(foreach)
-  library(doParallel)
-  
   mtdat <- cbind(subject_data[subject,,drop=F], sample_data)
   # Error out on NA metadata rather than allowing adonis to error out with a totally
   # nonsensical error message
@@ -84,23 +79,26 @@ PERMANOVA_repeat_measures <- function(
     stop("Some metadata is NA! adonis does not support any NA in the metadata")
   }
   # Test statistic from non-permuted data
-  ad <- adonis(D ~ ., permutations=0, data=mtdat[, metadata_order, drop=F])
+  ad <- vegan::adonis(D ~ ., permutations=0, data=mtdat[, metadata_order, drop=F])
   R2 <- ad$aov.tab$R2
   names(R2) <- rownames(ad$aov.tab)
   
-  registerDoParallel(ncores)
-  nullsamples <- foreach(i = seq_len(permutations),
-                         .combine = cbind) %dopar%
+  library(permute)
+  doParallel::registerDoParallel(ncores)
+  nullsamples <- foreach::`%dopar%`(
+    foreach::foreach(i = seq_len(permutations), .combine = cbind),
     {
       subject.i <- sample(unique(subject))
-      sample.i <- shuffle(nrow(sample_data), control=how(blocks=subject_permute))
-      mtdat <- cbind(subject_data[subject.i,,drop=F][subject,,drop=F],
+      sample.i <- shuffle(nrow(sample_data), 
+                          control = how(blocks=subject_permute))
+      i.subject_data <- subject_data[subject.i,,drop=F]
+      rownames(i.subject_data) <- rownames(subject_data)
+      mtdat <- cbind(i.subject_data[subject,,drop=F],
                      sample_data[sample.i,,drop=F])
-      perm.ad <- adonis(D ~ ., permutations=0, data=mtdat[, metadata_order, drop=F])
+      perm.ad <- vegan::adonis(D ~ ., permutations=0, data=mtdat[, metadata_order, drop=F])
       return(perm.ad$aov.tab$R2)
-    }
-  stopImplicitCluster()
-  
+    })
+  doParallel::stopImplicitCluster()
   
   # For residuals, test the other direction (i.e. p-value of all covariates)
   n <- length(R2)
@@ -143,12 +141,12 @@ PERMANOVA_repeat_measures_meta <- function(
   metadata_order = c(names(subject_data), names(sample_data)),
   permutations=999, ncores=1)
 {
-
+  
   # Make sure D is a dist object
   if (class(D) != "dist") {
     stop("D must be a dist object")
   }
-
+  
   # check sample identifiers in D, study, and subject
   if(nrow(as.matrix(D)) != length(study) |
      nrow(as.matrix(D)) != length(subject))
@@ -156,7 +154,7 @@ PERMANOVA_repeat_measures_meta <- function(
   if(any(rownames(as.matrix(D)) != names(study)) |
      any(rownames(as.matrix(D)) != names(subject)))
     stop("sample identifiers from D, study, and subject must match!")
-
+  
   # check study data
   if(!is.character(study))
     stop("study identifiers must be character!")
@@ -166,12 +164,12 @@ PERMANOVA_repeat_measures_meta <- function(
     stop("study number from study and study_longitudinal must match!")
   if(!setequal(study, names(study_longitudinal)))
     stop("study identifiers from study and study_longitudinal must match!")
-
+  
   # subject_data and sample_data cannot both be missing
   if(is.null(subject_data) & is.null(sample_data)) {
     stop("At least one of subject_data and sample_data must be provided!")
   }
-
+  
   # check subject data, create if missing
   if(!is.character(subject))
     stop("subject must be character!")
@@ -183,7 +181,7 @@ PERMANOVA_repeat_measures_meta <- function(
     stop("subject number from subject and subject_data must match!")
   if(!setequal(subject, rownames(subject_data)))
     stop("subject identifiers from subject and subject_data must match!")
-
+  
   # study identifiers and subject identifiers cannot overlap for the funciton to work
   if(length(intersect(study, subject)) > 0)
     stop("study identifiers and subject identifiers cannot overlap!")
@@ -195,7 +193,7 @@ PERMANOVA_repeat_measures_meta <- function(
   study_subject <- study[!duplicated(subject)]
   names(study_subject) <- subject[!duplicated(subject)]
   study_permute <- study_subject[rownames(subject_data)]
-
+  
   # create sample data if missing
   if(is.null(sample_data)) {
     sample_data <- data.frame(place_holder_sample = rep(1, nrow(as.matrix(D))),
@@ -211,18 +209,19 @@ PERMANOVA_repeat_measures_meta <- function(
   subject_permute <- ifelse(study_longitudinal[study],
                             subject,
                             study)
-
+  names(subject_permute) <- names(study)
+  
   # Ensure no metadata overlap between subject_data and sample_data
   if(length(intersect(names(subject_data), names(sample_data))) > 0) {
     stop("metadata is repeated across subject_data and sample_data")
   }
-
+  
   # Ensure that metadata_order only contains stuff in subject_data and sample_data
   if(length(setdiff(metadata_order, union(names(subject_data), names(sample_data)))) > 0) {
     stop("metadata_order contains metadata not in subject_data and sample_data!")
   }
-
-
+  
+  
   # Warn on some suspicious input
   # persample <- apply(sample_data, 1, function(x)is.factor(x) && !any(duplicated(x)))
   # if (any(persample)) {
@@ -234,12 +233,7 @@ PERMANOVA_repeat_measures_meta <- function(
   # if (!any(duplicated(subject))) {
   #   warning("subject contains no duplicated elements")
   # }
-
-  library(vegan)
   library(permute)
-  library(foreach)
-  library(doParallel)
-
   mtdat <- cbind(subject_data[subject,,drop=F], sample_data)
   # Error out on NA metadata rather than allowing adonis to error out with a totally
   # nonsensical error message
@@ -247,36 +241,40 @@ PERMANOVA_repeat_measures_meta <- function(
     stop("Some metadata is NA! adonis does not support any NA in the metadata")
   }
   # Test statistic from non-permuted data
-  ad <- adonis(D ~ ., permutations=0, data=mtdat[, metadata_order, drop=F])
+  ad <- vegan::adonis(D ~ ., permutations=0, data=mtdat[, metadata_order, drop=F])
   R2 <- ad$aov.tab$R2
   names(R2) <- rownames(ad$aov.tab)
-
-  registerDoParallel(ncores)
-  nullsamples <- foreach(i = seq_len(permutations),
-                         .combine = cbind) %dopar%
+  
+  doParallel::registerDoParallel(ncores)
+  nullsamples <- foreach::`%dopar%`(
+    foreach::foreach(i = seq_len(permutations), .combine = cbind),
     {
-      subject.i <- shuffle(nrow(subject_data), control=how(blocks=study_permute))
-      sample.i <- shuffle(nrow(sample_data), control=how(blocks=subject_permute))
-      mtdat <- cbind(subject_data[subject.i,,drop=F][subject,,drop=F],
+      subject.i <- shuffle(nrow(subject_data), 
+                           control = how(blocks = study_permute))
+      sample.i <- shuffle(nrow(sample_data), 
+                          control = how(blocks = subject_permute))
+      i.subject_data <- subject_data[subject.i,,drop=F]
+      rownames(i.subject_data) <- rownames(subject_data)
+      mtdat <- cbind(i.subject_data[subject,,drop=F],
                      sample_data[sample.i,,drop=F])
-      perm.ad <- adonis(D ~ ., permutations=0, data=mtdat[, metadata_order, drop=F])
+      perm.ad <- vegan::adonis(D ~ ., permutations=0, data=mtdat[, metadata_order, drop=F])
       return(perm.ad$aov.tab$R2)
-    }
-  stopImplicitCluster()
-
-
+    })
+  doParallel::stopImplicitCluster()
+  
+  
   # For residuals, test the other direction (i.e. p-value of all covariates)
   n <- length(R2)
   R2[n-1] <- 1 - R2[n-1]
   nullsamples[n-1,] <- 1 - nullsamples[n-1,]
-
+  
   # P value calculation similar to adonis's
   exceedances <- rowSums(nullsamples > R2)
   P <- (exceedances + 1) / (permutations + 1)
-
+  
   P[n] <- NA    # No p-values for "Total"
   ad$aov.tab$`Pr(>F)` <- P
-
+  
   return (ad)
 }
 #' 
