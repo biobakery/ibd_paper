431118a2fd2f123c7f29df79118500bb0e80a343
Siyuan Ma <sim653@mail.harvard.edu>
2019-04-24 17:02:35 -0400

    update simulation schema for unsupervised sections. update summarise Rmds

--- functions/simulations/color_scheme.R
+++ functions/simulations/color_scheme.R
@@ -1,6 +1,11 @@
 colors_basis <- gg_color_hue(n=3)
+colors_base <- c("Original" = "black",
+                 "MMUPHin corrected" = colors_basis[1])
 colors_simulation_adjust.batch <- c("Original" = "black",
                                     "ComBat corrected" = colors_basis[3],
                                     "MMUPHin corrected" = colors_basis[1])
-colors_simulation_unsupervised <- c("Original" = "black",
-                                    "MMUPHin corrected" = colors_basis[1])
+colors_simulation_lm.meta <- c("Original, naive analyzed" = "black",
+                               "MMUPHin corrected, naive analyzed" = "brown",
+                               "MMUPHin corrected, MMUPHin analyzed" = colors_basis[1])
+colors_simulation_continuous <- c("Original, MMUPHin analyzed" = "brown",
+                                  "MMUPHin corrected, MMUPHin analyzed" = colors_basis[1])
