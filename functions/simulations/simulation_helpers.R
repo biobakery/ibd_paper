78ebe4a12adba8ac543709c045b8a7e52e5d4dde
Siyuan Ma <sim653@mail.harvard.edu>
2019-07-02 22:31:31 -0400

    add additional helper function

--- functions/simulations/simulation_helpers.R
+++ functions/simulations/simulation_helpers.R
@@ -6,6 +6,18 @@ create_batch <- function(nSample_perBatch, nBatch) {
   return(batch.samples)
 }
 
+create_imbalancedBatches <- function(nSample_perBatch, nBatch, imbalance) {
+  if(nBatch != 2) 
+    stop("Currently only support 2 batches!")
+  batch <- create_batch(nSample_perBatch, nBatch)
+  exposure <- rep(NA_real_, length = length(batch))
+  exposure[batch == 1] <- rbinom(n = nSample_perBatch, size = 1, prob = 0.5 - imbalance)
+  exposure[batch == 2] <- rbinom(n = nSample_perBatch, size = 1, prob = 0.5 + imbalance)
+  data.frame(batch = as.factor(batch),
+             exposure = as.factor(exposure),
+             row.names = paste0("Sample", 1:(nSample_perBatch * nBatch)))
+}
+
 create_continuousStructure <- function(nSample_perBatch, nBatch) {
   batch <- create_batch(nSample_perBatch, nBatch)
   score <- runif(nSample_perBatch * nBatch, min = -1, max = 1)
