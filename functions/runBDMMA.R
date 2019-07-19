c722a0e144d798fd47189fefbc20aa91c9e20e46
Siyuan Ma <sim653@mail.harvard.edu>
2019-07-19 13:48:31 -0400

    do things work now?

--- functions/runBDMMA.R
+++ functions/runBDMMA.R
@@ -1,26 +1,23 @@
 library(magrittr)
 ii <- commandArgs(trailingOnly = TRUE) %>% as.integer()
-print(ii)
 load("/n/hutlab11_nobackup/users/syma/ibd_paper/results/simulations/lm.meta/tb_sim_subset.RData")
-i <- tb_sim_subset$i[ii]
+i_simSetup <- tb_sim_subset[ii, ]
+df_metadata <- i_simSetup$df_metadata[[1]] %>%
+  dplyr::transmute(main = as.numeric(as.character(exposure)),
+                   confounder = NA_real_,
+                   batch = as.numeric(as.character(batch)))
+i <- i_simSetup$i[ii]
 mat_otu <- read.table(paste0("/n/hutlab11_nobackup/users/syma/ibd_paper/results/simulations/lm.meta/sparseDOSSA_sets/",
-                      ii, ".tsv"),
+                             i, ".tsv"),
                       header = TRUE,
                       sep = "\t",
                       row.names = 1) %>%
   as.matrix()
-print(ii)
+sSet <- SummarizedExperiment::SummarizedExperiment(
+  assays = list(mat_otu),
+  colData = df_metadata
+)
 
-# i_simSetup <- tb_sim[i, ]
-# df_metadata <- i_simSetup$df_metadata[[1]] %>% 
-#   dplyr::transmute(main = as.numeric(as.character(exposure)),
-#                    confounder = NA_real_,
-#                    batch = as.numeric(as.character(batch)))
-# sSet <- SummarizedExperiment::SummarizedExperiment(
-#   assays = list(mat_otu),
-#   colData = df_metadata
-# )
-# 
 # fit.BDMMA <- BDMMAcorrect::BDMMA(Microbiome_dat = sSet)
-# save(sSet, file = paste0("/n/hutlab11_nobackup/users/syma/ibd_paper/results/simulations/lm.meta/BDMMA/",
-#                          i, ".RData"))
+save(sSet, file = paste0("/n/hutlab11_nobackup/users/syma/ibd_paper/results/simulations/lm.meta/BDMMA/",
+                         i, ".RData"))
