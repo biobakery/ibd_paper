0ac74c0d0d8fcc1df7968f9700f9171b93e29af3
Siyuan Ma <sim653@mail.harvard.edu>
2019-07-19 13:28:50 -0400

    is it tb_sim is too large?

--- functions/runBDMMA.R
+++ functions/runBDMMA.R
@@ -1,20 +1,19 @@
 library(magrittr)
 ii <- commandArgs(trailingOnly = TRUE) %>% as.integer()
 # print(ii)
-load("/n/hutlab11_nobackup/users/syma/ibd_paper/results/simulations/lm.meta/tb_sim.RData")
-tb_sim_subset <- tb_sim %>%
-  dplyr::filter(nSample_perBatch == 100,
-                nMicrobe == 200,
-                spikeMicrobes == 0.05)
-i <- tb_sim_subset$i[ii]
+# load("/n/hutlab11_nobackup/users/syma/ibd_paper/results/simulations/lm.meta/tb_sim.RData")
+# tb_sim_subset <- tb_sim %>%
+#   dplyr::filter(nSample_perBatch == 100,
+#                 nMicrobe == 200,
+#                 spikeMicrobes == 0.05)
+# i <- tb_sim_subset$i[ii]
 mat_otu <- read.table(paste0("/n/hutlab11_nobackup/users/syma/ibd_paper/results/simulations/lm.meta/sparseDOSSA_sets/",
                       i, ".tsv"),
                       header = TRUE,
                       sep = "\t",
                       row.names = 1) %>%
   as.matrix()
-
-print(i)
+print(ii)
 
 # i_simSetup <- tb_sim[i, ]
 # df_metadata <- i_simSetup$df_metadata[[1]] %>% 
