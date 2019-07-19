62defd9bbc5e49e13ee8a4464a52f89249f244f7
Siyuan Ma <sim653@mail.harvard.edu>
2019-07-19 12:53:01 -0400

    bdmma script

--- functions/manuscript/suppFigures/suppFig_simulation_lm.R
+++ functions/manuscript/suppFigures/suppFig_simulation_lm.R
@@ -2,100 +2,160 @@ load("results/simulations/lm.meta/tb_sim.RData")
 load("results/simulations/lm.meta/evaluate_before.RData")
 load("results/simulations/lm.meta/evaluate_after.RData")
 load("results/simulations/lm.meta/evaluate_qnorm.RData")
-load("results/simulations/lm.meta/evaluate_BDMMA.RData")
 
-suppFig_simulation_adjBatch <- 
-  function(tb_sim, results_before, results_correct, results_ComBat,
-           path = "supp_materials/suppFigures/suppFig_simulation_adjBatch.pdf") {
-    # effect sizes
-    tb_sim <- tb_sim %>% 
-      dplyr::mutate(effect_poscontrol = effectSize %>% 
-                      purrr::map_dbl("positive_control1"),
-                    effect_batch = effectSize %>% 
-                      purrr::map_dbl("batch") %>% 
-                      forcats::as_factor() %>% forcats::fct_inseq())
-    # Format parameter table
-    # nSample and nBatch
-    tb_sim <- tb_sim %>% 
-      dplyr::arrange(nSample_perBatch, nBatch) %>% 
-      dplyr::mutate(nSample_nBatch = paste0("nSample per batch = ", nSample_perBatch, ", ",
-                                            "nBatch = ", nBatch) %>% 
-                      forcats::as_factor())
-    # nMicrobe and percMicrobe spiked
-    tb_sim <- tb_sim %>% 
-      dplyr::arrange(nMicrobe, spikeMicrobes) %>% 
-      dplyr::mutate(nMicrobe_percSpike = paste0("nFeature = ", nMicrobe, ", ",
-                                                spikeMicrobes*100, "% spiked") %>% 
-                      forcats::as_factor())
-    
-    # summarize R2s
-    tb_R2 <- list(results, results_ComBat, results_correct) %>% 
-      purrr::map2_dfr(
-        c("Original", "ComBat corrected", "MMUPHin corrected"),
-        ~ .x %>% 
-          purrr::imap_dfr(
-            ~ tibble::tibble(Variable = names(.x$R2),
-                             R2 = .x$R2,
-                             i = .y)
-          ) %>% 
-          dplyr::mutate(Data = .y)
-      ) 
-    # format variables
-    tb_R2 <- tb_R2 %>% 
-      dplyr::mutate(
-        Variable = Variable %>% 
-          dplyr::recode_factor(
-            "batch" = "Batch",
-            "positive_control1" = "Positive control (binary)",
-            "positive_control2" = "Positive control (continuous)",
-            "negative_control" = "Negative control"
-          ),
-        Data = factor(Data, levels = c("Original",
-                                       "ComBat corrected",
-                                       "MMUPHin corrected"))
-      )
-    
-    # colors for the mapped methods
-    colors <- smar::gg_color_hue(n=4)
-    colors_simulation_adjust.batch <- c("Original" = "black",
-                                        "ComBat corrected" = colors[2],
-                                        "MMUPHin corrected" = colors[1])
-    
-    tb_plot <- tb_R2 %>% dplyr::left_join(tb_sim, by = "i")
-    plist <- (1:nlevels(tb_plot$Variable)) %>% 
-      purrr::map(function(i) {
-        i.Variable <- levels(tb_plot$Variable)[i]
-        p <- tb_plot %>% 
-          dplyr::filter(Variable == i.Variable) %>% 
-          ggplot(aes(x = effect_batch, y = R2, color = Data)) +
-          geom_boxplot() +
-          facet_grid(nMicrobe_percSpike ~ nSample_nBatch,
-                     scales = "free_y") +
-          scale_color_manual(values = colors_simulation_adjust.batch) +
-          theme(legend.position = c(0, 1),
-                legend.justification = c(0, 1),
-                legend.background = element_blank(),
-                strip.text = element_text(size = 8),
-                legend.text = element_text(size = 8)) +
-          xlab("Simulatd strength of batch effect") +
-          ggtitle(i.Variable)
-        if(i >= 2)
-          p <- p + theme(legend.position = "none")
-        return(p)
-      })
-    
-    cowplot::plot_grid(plotlist = plist,
-                       nrow = 2,
-                       labels = c("a", "b", "c", "d"),
-                       label_fontface = "plain",
-                       label_size = 20) %>% 
-      ggsave(path,
-             .,
-             width = 50, height = 30, limitsize = FALSE)
-    return(plist)
-  }
+load("results/simulations/lm.meta/time_MMUPHin.RData")
+load("results/simulations/lm.meta/time_BDMMA.RData")
+times_qnorm <- readr::read_tsv("results/simulations/lm.meta/qnorm/time_qnorm.txt", col_names = FALSE) %>% 
+  dplyr::transmute(`Computation time (min)` = X1 / 60,
+                   Class = "Quantile normalization") %>% 
+  dplyr::slice(-1)
+tb_time <- rbind(
+  times_qnorm,
+  times_BDMMA %>% 
+    purrr::map_dbl(~ as.double(.x, units = "mins")) %>% 
+    {tibble::tibble(`Computation time (min)` = .,
+                    Class = "BDMMA")},
+  times_MMUPHin %>% 
+    purrr::map_dbl(~ as.double(.x, units = "mins")) %>% 
+    {tibble::tibble(`Computation time (min)` = .,
+                    Class = "MMUPHin")}
+) %>% 
+  dplyr::mutate(Class = Class %>% forcats::as_factor())
 
-ps <- suppFig_simulation_adjBatch(tb_sim = tb_sim, 
-                                  results_before = results_before,
-                                  results_correct = results_correct,
-                                  results_ComBat = results_ComBat)
+suppFig_simulation_lm <- function(tb_sim, 
+                                  evaluate_before, evaluate_after, 
+                                  evaluate_qnorm, tb_time,
+                                  path = "supp_materials/suppFigures/suppFig_simulation_lm.pdf") {
+  # Format results
+  # effect sizes
+  tb_sim <- tb_sim %>% 
+    dplyr::mutate(effect_exposure = imbalance %>% 
+                    forcats::as_factor() %>% forcats::fct_inseq(),
+                  effect_batch = effectSize %>%
+                    purrr::map_dbl("batch") %>% 
+                    forcats::as_factor() %>% forcats::fct_inseq())
+  # nSample and nBatch
+  tb_sim <- tb_sim %>% 
+    dplyr::arrange(nSample, nBatch) %>% 
+    dplyr::mutate(nSample_nBatch = paste0("nSample per batch = ", nSample_perBatch, ", ",
+                                          "nBatch = ", nBatch) %>% 
+                    forcats::as_factor())
+  # nMicrobe and percMicrobe spiked
+  tb_sim <- tb_sim %>% 
+    dplyr::arrange(nMicrobe, spikeMicrobes) %>% 
+    dplyr::mutate(nMicrobe_percSpike = paste0("nFeature = ", nMicrobe, ", ",
+                                              spikeMicrobes*100, "% spiked") %>% 
+                    forcats::as_factor())
+  
+  # aggregate results into df format
+  tb_results <- list(results, results_correct) %>% 
+    purrr::map2_dfr(
+      c("Original", "MMUPHin corrected"),
+      ~ .x %>% 
+        purrr::imap_dfr(
+          function(result, i_result) {
+            if(.y == "Original")
+              return(tibble::tibble(model = "naive",
+                                    results = result["naive"],
+                                    i = i_result))
+            if(.y == "MMUPHin corrected")
+              return(tibble::tibble(model = "MMUPHin",
+                                    results = result["MMUPHin"],
+                                    i = i_result))
+          }) %>% 
+        dplyr::mutate(Data = .y)
+    ) 
+  tb_results_qnorm <- results_qnorm %>% 
+    purrr::imap_dfr(
+      ~ tibble::tibble(results = list(.x),
+                       i = .y)
+    ) %>% 
+    dplyr::mutate(Data = "Quantile corrected",
+                  model = "Wilcoxon")
+  # format variables
+  tb_results <- rbind(tb_results, tb_results_qnorm) %>% 
+    dplyr::mutate(Data = factor(Data, levels = c("Original", 
+                                                 "Quantile corrected",
+                                                 "MMUPHin corrected")),
+                  model = factor(model, levels = c("naive", "Wilcoxon", "MMUPHin"))) %>% 
+    dplyr::arrange(Data, model) %>% 
+    dplyr::mutate(Class = paste0(Data, ", ", model, " analyzed") %>% 
+                    dplyr::recode("Original, naive analyzed" = "Naive model",
+                                  "Quantile corrected, Wilcoxon analyzed" = "Quantile normalization",
+                                  "BDMMA" = "BDMMA",
+                                  "MMUPHin corrected, MMUPHin analyzed" = "MMUPHin") %>% 
+                    forcats::as_factor())
+  
+  # calculate FPR
+  tb_results <- tb_results %>% 
+    dplyr::left_join(tb_sim, by = "i") %>% 
+    dplyr::mutate(FPR = results %>% 
+                    purrr::map_dbl(~sum(.x$pval < 0.05, na.rm = TRUE)) %>% 
+                    magrittr::divide_by(nMicrobe))
+  # summarise mean and sd FPR across replicates
+  tb_summary <- tb_results %>%
+    dplyr::group_by(i_setup, Class) %>% 
+    dplyr::summarise(mean_FPR = mean(FPR),
+                     sd_FPR = sd(FPR)) %>% 
+    dplyr::ungroup()
+  
+  # Set up colors
+  colors <- smar::gg_color_hue(n=4)
+  colors_simulation_lm.meta <- c("Naive model" = "black",
+                                 "Quantile normalization" = colors[3],
+                                 "BDMMA" = colors[4],
+                                 "MMUPHin" = colors[1])
+  
+  tb_plot <- tb_summary %>% 
+    dplyr::left_join(dplyr::filter(tb_sim, !duplicated(i_setup)), 
+                     by = "i_setup")
+  plist <- (1:nlevels(tb_sim$effect_exposure)) %>% 
+    purrr::map(function(i) {
+      i.effect <- levels(tb_sim$effect_exposure)[i]
+      p <- tb_plot %>% 
+        dplyr::filter(effect_exposure == i.effect) %>% 
+        ggplot(aes(x = effect_batch, y = mean_FPR, color = Class)) +
+        geom_point(size = 3, position = position_dodge(width = 1)) +
+        geom_errorbar(aes(ymin = mean_FPR - sd_FPR, ymax = mean_FPR + sd_FPR),
+                      position = position_dodge(width = 1)) +
+        geom_hline(yintercept = 0.05, linetype = "dashed") +
+        facet_grid(nMicrobe_percSpike ~ nSample_nBatch) +
+        scale_color_manual(values = colors_simulation_lm.meta) +
+        theme(legend.position = c(1, 1),
+              legend.justification = c(1, 1),
+              legend.background = element_blank(),
+              legend.title = element_blank(),
+              strip.text = element_text(size = 8),
+              legend.text = element_text(size = 8)) +
+        xlab("Batch effect") +
+        ylab("False positive rate") +
+        ggtitle(paste0("Exposure imbalance = ", 
+                       i.effect %>% 
+                         as.numeric %>% 
+                         magrittr::multiply_by(200),
+                       "%"))
+      if(i != 3)
+        p <- p + theme(legend.position = "none")
+      return(p)
+    })
+  
+  p_time <- tb_time %>% 
+    ggplot(aes(x = Class, y = `Computation time (min)`)) +
+    geom_boxplot() +
+    ylab("Computation time (min)") +
+    theme(axis.title.x = element_blank()) +
+    ggtitle("Computation performance comparison")
+  
+  cowplot::plot_grid(plotlist = c(plist,
+                                  cowplot::plot_grid(p_time, NULL, nrow = 1,
+                                                     rel_widths = c(2, 1)) %>% 
+                                    cowplot::plot_grid(NULL, ncol = 1,
+                                                       rel_heights = c(1, 3)) %>% 
+                                    list()),
+                     nrow = 2) %>% 
+    ggsave(path,
+           .,
+           width = 30, height = 30)
+}
+
+
--- functions/runBDMMA.R
+++ functions/runBDMMA.R
@@ -1,4 +1,6 @@
 library(magrittr)
+ii <- commandArgs(trailingOnly = TRUE) %>% as.integer()
+
 load("/n/hutlab11_nobackup/users/syma/ibd_paper/results/simulations/lm.meta/tb_sim.RData")
 tb_sim_subset <- tb_sim %>% 
   dplyr::filter(nSample_perBatch == 100,
@@ -10,17 +12,20 @@ mat_otu <- read.table(paste0("/n/hutlab11_nobackup/users/syma/ibd_paper/results/
                       header = TRUE,
                       sep = "\t",
                       row.names = 1) %>%
-  as.matrix
-i_simSetup <- tb_sim[i, ]
-df_metadata <- i_simSetup$df_metadata[[1]] %>% 
-  dplyr::transmute(main = as.numeric(as.character(exposure)),
-                   confounder = NA_real_,
-                   batch = as.numeric(as.character(batch)))
-sSet <- SummarizedExperiment::SummarizedExperiment(
-  assays = list(mat_otu),
-  colData = df_metadata
-)
+  as.matrix()
 
-fit.BDMMA <- BDMMAcorrect::BDMMA(Microbiome_dat = sSet)
-save(sSet, file = paste0("/n/hutlab11_nobackup/users/syma/ibd_paper/results/simulations/lm.meta/BDMMA/",
-                         i, ".RData"))
+print(i)
+
+# i_simSetup <- tb_sim[i, ]
+# df_metadata <- i_simSetup$df_metadata[[1]] %>% 
+#   dplyr::transmute(main = as.numeric(as.character(exposure)),
+#                    confounder = NA_real_,
+#                    batch = as.numeric(as.character(batch)))
+# sSet <- SummarizedExperiment::SummarizedExperiment(
+#   assays = list(mat_otu),
+#   colData = df_metadata
+# )
+# 
+# fit.BDMMA <- BDMMAcorrect::BDMMA(Microbiome_dat = sSet)
+# save(sSet, file = paste0("/n/hutlab11_nobackup/users/syma/ibd_paper/results/simulations/lm.meta/BDMMA/",
+#                          i, ".RData"))
